02b74225946ced89834ace3bc99a306cdb484092
Mr. KHC <mr.khc0626@gmail.com>
2023-11-17 16:26:58 +1100

    Set Health Connection for testing

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,8 @@
-exports.printMsg = function () {
-    console.log("Library is connected.");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.health = void 0;
+const health = () => {
+    return 'OK';
 };
+exports.health = health;
 //# sourceMappingURL=index.js.map
